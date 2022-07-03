a0e83e2ad1dfcce63b355eb15ce8b82d822dd2ef
chimpanstache <53328741+chimpanstache@users.noreply.github.com>
2022-07-03 22:13:48 +0200

    size with tests

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -19,10 +19,22 @@ RSpec.describe LinkedList do
 
     list.prepend('1st node')
     list.prepend('2nd node')
-    it 'adds a new node at the beginning of the list' do
+    it 'adds a new node at the start of the list' do
       expect(list.size).to eq 2
       expect(list.head.value).to eq '2nd node'
       expect(list.tail.value).to eq '1st node'
+    end
+  end
+
+  describe '#size' do
+    list = LinkedList.new
+  
+    it 'returns the current number of nodes contained in the list' do
+      expect(list.size).to eq 0
+      list.append('blabla')
+      expect(list.size).to eq 1
+      list.append('more blabla')
+      expect(list.size).to eq 2
     end
   end
 end
